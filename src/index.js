50b40fee8da9e5e8a427c6e466bb0a01a8c7d67c
David Kiki <david.kariuki2@student.moringaschool.com>
2025-03-18 15:51:22 +0300

    Completed tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#new-task-description");
+    if (input.value.trim() === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    taskList.appendChild(li);
+    
+    input.value = ""; // Clear input field after submission
+  });
 });
